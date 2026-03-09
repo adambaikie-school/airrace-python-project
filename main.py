9af53430a142a5b4b5c211694743a0cb387102ce
Adam <24091@wc.school.nz>
2026-03-10 12:30:28 +1300

    Added clear fnc

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
-#Varriables
+# Imports
+import os
+import time
 
-#loops
+
+# Variables
+
+
+# Constant Variables
+
+
+# Functions
+
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+# loops
 while True:
-    print("Test")
+    print("Welcome to Air Race! \n")
+    print("The aim of this game, is to race across the world, the fastest way possible! \n")
+    print("However, along the way you may discover problems and find ways to fix them.\n")
+    answer = input("Are you ready to race? Yes/No: ")
+    if answer not in ("yes", "y", "Yes", "Y"):
+        clear_console()
+        print("Okay! Hope your ready next time!")
+        time.sleep(3)
+        continue
+    print("Here are the controls:\n")
+    print("You will be using the numbers 1,2,3,4,5 to choose your path.")
+    print("Use 'A' and 'D' when prompted!")
+
+
+
+
